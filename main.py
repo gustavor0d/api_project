56c7b43fc86bea41cc03456dfeb892ae94985983
Gustavo Rodrigues <gustavorodriguesoliveira2013@gmail.com>
2025-03-20 14:46:04 +0000

    fix: change main server port from 5002 to 5000

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from routes.alunos import alunos_bp, alunos
+from routes.professores import professores_bp, professores
+from routes.turmas import turmas_bp, turmas
 
 app = Flask(__name__)
 
+app.register_blueprint(alunos_bp)
+app.register_blueprint(professores_bp)
+app.register_blueprint(turmas_bp)
 
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+@app.route("/")
+def homepage():
+    return "Página raiz"
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+@app.route("/reseta", methods=['POST'])
+def reset_server():
+    alunos.clear()
+    professores.clear()
+    turmas.clear()
+    return jsonify(message="resetado com sucesso")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
